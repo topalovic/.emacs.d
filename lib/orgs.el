8c935a880d75dbe41a4b3097eee66d26aee165da
Nikola Topalović <nikola.topalovic@gmail.com>
2021-04-27 17:05:59 +0200

    Some global defaults for org exports

--- lib/orgs.el
+++ lib/orgs.el
@@ -43,9 +43,19 @@
 
 ;; latex
 
+;; to be inserted in every latex header
+(setq org-latex-packages-alist
+      '(("" "indentfirst" nil)))
+
 (setq org-preview-latex-default-process 'imagemagick)
 
 (setq org-format-latex-options
       (plist-put org-format-latex-options :scale 1.4))
+
+(setq org-latex-hyperref-template
+      "\\hypersetup{pdfauthor={%a},pdftitle={%t},hidelinks}")
+
+(setq org-export-with-author nil)
+(setq org-export-with-date nil)
 
 (provide 'orgs)
