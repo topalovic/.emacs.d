28490c012523210438edfd4d59cdf5e51af44416
Nikola Topalović <nikola.topalovic@gmail.com>
2021-06-21 14:37:14 +0200

    🌈

--- lib/development.el
+++ lib/development.el
@@ -64,6 +64,14 @@
   :config
   (smartparens-global-mode 1))
 
+(use-package rainbow-delimiters
+  :config
+  (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
+  (add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
+  (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+  (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
+  (add-hook 'scheme-mode-hook 'rainbow-delimiters-mode))
+
 (use-package smart-compile
   :bind
   (("C-c c" . smart-compile)))
