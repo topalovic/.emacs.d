3b01ac447ca6047d23efa0fa1862189c6c6151fb
Nikola Topalović <nikola.topalovic@gmail.com>
2021-09-27 18:09:22 +0200

    Update package archive URLs and fix the TLS issue

--- lib/boot.el
+++ lib/boot.el
@@ -4,10 +4,11 @@
 
 (setq package-enable-at-startup nil)
 
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
 (setq package-archives
       '(("gnu"   . "https://elpa.gnu.org/packages/")
-        ("melpa" . "https://melpa.org/packages/")
-        ("org"   . "https://orgmode.org/elpa/")))
+        ("melpa" . "https://melpa.org/packages/")))
 
 (package-initialize)
 
