143953e90372ed23d99492a1fa3e767f17a5106d
Nikola Topalović <nikola.topalovic@gmail.com>
2024-01-07 10:19:34 +0100

    Bind kill-whole-line

--- lib/keys.el
+++ lib/keys.el
@@ -18,6 +18,7 @@
 ;; marking & killing
 (global-set-key (kbd "M-o") 'mark-whole-word)
 (global-set-key (kbd "C-w") 'backward-kill-word)
+(global-set-key (kbd "C-S-k") 'kill-whole-line)
 (global-set-key (kbd "C-x C-k") 'kill-region)
 (global-set-key (kbd "C-c C-k") 'kill-region)
 
